952afba2a7c84f8614f3ec17aa6c643ad67623b2
Bartek <yedenasty@gmail.com>
2017-03-08 20:27:01 +0100

    dodana lista wyszukanych wynikow

--- app/src/main/java/com/example/rent/filmbrowserapp/ListingPresenter.java
+++ app/src/main/java/com/example/rent/filmbrowserapp/ListingPresenter.java
@@ -9,8 +9,20 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 import nucleus.presenter.Presenter;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ListingPresenter extends Presenter<ListingActivity> {
+
+    private Retrofit retrofit;
+    public ListingPresenter() {
+        retrofit = new Retrofit.Builder()
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create())
+                .baseUrl("https://www.omdbapi.com/")
+                .build();
+    }
 
     public void getDataAsync(String title){
         new Thread() {
---
+++ app/src/main/java/com/example/rent/filmbrowserapp/SearchService.java
@@ -0,0 +1,8 @@
+package com.example.rent.filmbrowserapp;
+
+/**
+ * Created by RENT on 2017-03-08.
+ */
+
+public class SearchService {
+}
